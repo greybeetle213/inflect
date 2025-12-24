ede5c1a523226c9351c7c265b88b81aad69d019b
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2025-12-25 10:29:51 +1300

    Add files via upload

--- sw.js
+++ sw.js
@@ -32,6 +32,36 @@ const putInCache = async (request, response) => {
     const cache = await caches.open("v1");
     await cache.put(request, response);
 };
+
+const netFirst = async (request, preloadResponsePromise, event) => {
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        event.waitUntil(putInCache(request, responseFromNetwork.clone()));
+        return responseFromNetwork;
+    } catch (error) {
+
+    }
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+
+    // Next try to use (and cache) the preloaded response, if it's there
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+        console.info("using preload response", preloadResponse);
+        event.waitUntil(putInCache(request, preloadResponse.clone()));
+        return preloadResponse;
+    }
+
+    // Next try to get the resource from the network
+    
+};
+
 const cacheFirst = async (request, preloadResponsePromise, event) => {
     // First try to get the resource from the cache
     const responseFromCache = await caches.match(request);
@@ -102,11 +132,22 @@ self.addEventListener("install", (event) => {
 })
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      cacheFirst(
-        event.request,
-        event.preloadResponse,
-        event,
-      )
-    );
-  });
+    if(event.request && event.request.url.indexOf("dict")==-1){
+        event.respondWith(
+            netFirst(
+                event.request,
+                event.preloadResponse,
+                event
+            )
+        )
+    }else{
+        event.respondWith(
+            cacheFirst(
+                event.request,
+                event.preloadResponse,
+                event
+            )
+        );
+    }
+  }
+)
